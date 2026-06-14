005c31c1dbe75341874cebf5cc6a8760e7cbb32e
Ivan Llanos <ivan.llanos.santamaria@gmail.com>
2026-06-14 13:13:04 +1000

    add MovieGrid responsive layout with SCSS

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,3 +1,19 @@
-export default function MovieGrid() {
-  return null
+import type { Movie } from '@/types/tmdb'
+import MovieCard from '@/components/MovieCard/MovieCard'
+import styles from './MovieGrid.module.scss'
+
+interface Props {
+  movies: Movie[]
+}
+
+export default function MovieGrid({ movies }: Props) {
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.grid}>
+        {movies.map((movie, index) => (
+          <MovieCard key={movie.id} movie={movie} index={index} />
+        ))}
+      </div>
+    </section>
+  )
 }
