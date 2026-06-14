c56c20f4d8128ec7a614e38f7c3a151879565f75
Ivan Llanos <ivan.llanos.santamaria@gmail.com>
2026-06-14 13:54:58 +1000

    add genre filter dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,31 @@
-import { getPopularMovies } from '@/lib/tmdb'
+import { Suspense } from 'react'
+import { getGenres, getMoviesByGenre, getPopularMovies, searchMovies } from '@/lib/tmdb'
 import MovieGrid from '@/components/MovieGrid/MovieGrid'
+import SearchBar from '@/components/SearchBar/SearchBar'
 
 interface Props {
-  searchParams: Promise<{ page?: string }>
+  searchParams: Promise<{ page?: string; query?: string; genre?: string }>
 }
 
 export default async function Home({ searchParams }: Props) {
-  const { page: pageParam } = await searchParams
+  const { page: pageParam, query, genre } = await searchParams
   const page = pageParam ? Number(pageParam) : 1
-  const data = await getPopularMovies(page)
 
-  return <MovieGrid movies={data.results} />
+  const [data, genres] = await Promise.all([
+    genre
+      ? getMoviesByGenre(genre, page)
+      : query
+        ? searchMovies(query, page)
+        : getPopularMovies(page),
+    getGenres(),
+  ])
+
+  return (
+    <>
+      <Suspense>
+        <SearchBar genres={genres} />
+      </Suspense>
+      <MovieGrid movies={data.results} />
+    </>
+  )
 }
