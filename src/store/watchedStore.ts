208b5ef7aae9ed8db56debfbd17b7e3c9f7581a1
Ivan Llanos <ivan.llanos.santamaria@gmail.com>
2026-06-14 14:30:50 +1000

    feat: add Zustand watched store with localStorage persistence

--- src/store/watchedStore.ts
+++ src/store/watchedStore.ts
@@ -1,1 +1,44 @@
-export {}
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export interface WatchedMovie {
+  id: number
+  title: string
+  poster_path: string | null
+}
+
+interface WatchedStore {
+  watchedIds: number[]
+  watchedMovies: WatchedMovie[]
+  toggleWatched: (movie: WatchedMovie) => void
+  isWatched: (id: number) => boolean
+}
+
+export const useWatchedStore = create<WatchedStore>()(
+  persist(
+    (set, get) => ({
+      watchedIds: [],
+      watchedMovies: [],
+
+      toggleWatched: (movie: WatchedMovie) => {
+        const { watchedIds, watchedMovies } = get()
+        if (watchedIds.includes(movie.id)) {
+          set({
+            watchedIds: watchedIds.filter(id => id !== movie.id),
+            watchedMovies: watchedMovies.filter(m => m.id !== movie.id),
+          })
+        } else {
+          set({
+            watchedIds: [movie.id, ...watchedIds],
+            watchedMovies: [movie, ...watchedMovies],
+          })
+        }
+      },
+
+      isWatched: (id: number) => get().watchedIds.includes(id),
+    }),
+    {
+      name: 'martins-movies-watched',
+    }
+  )
+)
